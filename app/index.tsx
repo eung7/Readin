263d774db6ef2b9cbff92b52a362036a515e8cfc
Eunzo <wannabeeung@gmail.com>
2025-07-03 23:11:00 +0900

    💄 Add colors.ts

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
-import "./global.css";
+import "./styles/global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-2xl font-bold">Readin</Text>
+    <View className="flex-1 items-center justify-center bg-gray-500">
+      <Text className="text-2xl font-bold text-primary-500">Readin</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from "./app/styles/colors";
+
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+    },
   },
   plugins: [],
 };
